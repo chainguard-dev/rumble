fc8cd31b4d24469c9aeb11f9e4cc4d0a681260ff
John Speed Meyers <jsmeyers@chainguard.dev>
2023-01-13 14:29:29 -0500

    And the codebase was reborn
    Signed-off-by: John Speed Meyers <jsmeyers@chainguard.dev>

--- main.go
+++ main.go
@@ -3,20 +3,15 @@ package main
 import (
 	//"bufio"
 	///"bytes"
-	"context"
+
 	"flag"
 	"fmt"
 	"log"
-	"os"
-	"time"
-
-	trivyartifact "github.com/aquasecurity/trivy/pkg/commands/artifact"
-	trivyflag "github.com/aquasecurity/trivy/pkg/flag"
 )
 
 func main() {
 	image := flag.String("image", "cgr.dev/chainguard/static:latest", "OCI image")
-	scanner := flag.String("scanner", "trivy", "Which scanner to use, (\"trivy\" or \"grype\")")
+	scanner := flag.String("scanner", "grype", "Which scanner to use, (\"trivy\" or \"grype\")")
 	flag.Parse()
 	if err := scanImage(*image, *scanner); err != nil {
 		panic(err)
@@ -34,34 +29,7 @@ func scanImage(image string, scanner string) error {
 }
 
 func scanImageTrivy(image string) error {
-	log.Printf("scanning %s with trivy\n", image)
-	reportFlagGroup := trivyflag.NewReportFlagGroup()
-	reportFlagGroup.DependencyTree = nil
-	reportFlagGroup.ReportFormat = nil
-	imageFlags := &trivyflag.Flags{
-		CacheFlagGroup:         trivyflag.NewCacheFlagGroup(),
-		DBFlagGroup:            trivyflag.NewDBFlagGroup(),
-		ImageFlagGroup:         trivyflag.NewImageFlagGroup(),
-		LicenseFlagGroup:       trivyflag.NewLicenseFlagGroup(),
-		MisconfFlagGroup:       trivyflag.NewMisconfFlagGroup(),
-		RemoteFlagGroup:        trivyflag.NewClientFlags(),
-		RegoFlagGroup:          trivyflag.NewRegoFlagGroup(),
-		ReportFlagGroup:        reportFlagGroup,
-		ScanFlagGroup:          trivyflag.NewScanFlagGroup(),
-		SecretFlagGroup:        trivyflag.NewSecretFlagGroup(),
-		VulnerabilityFlagGroup: trivyflag.NewVulnerabilityFlagGroup(),
-	}
-	globalFlags := trivyflag.NewGlobalFlagGroup()
-	options, err := imageFlags.ToOptions("rumble-dev", []string{image}, globalFlags, os.Stderr)
-	if err != nil {
-		return err
-	}
-	options.GlobalOptions.Debug = true
-	options.Timeout = time.Minute * 5
-	options.ReportOptions.Format = "json"
-	//options.ReportOptions.ReportFormat = "json"
-	ctx := context.Background()
-	return trivyartifact.Run(ctx, options, trivyartifact.TargetContainerImage)
+	return nil
 }
 
 func scanImageGrype(image string) error {
