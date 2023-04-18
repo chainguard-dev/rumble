04bb8cdceb0c1daf15f54899fdec1d4c4cb83e94
Josh Dolitsky <josh@dolit.ski>
2023-04-18 15:22:35 -0500

    Add image build time to data
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

---
+++ pkg/oci/oci.go
@@ -0,0 +1,25 @@
+package oci
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+)
+
+func ImageBuildTime(imageRef string) (*time.Time, error) {
+	ref, err := name.ParseReference(imageRef)
+	if err != nil {
+		return nil, fmt.Errorf("parsing reference %q: %w", imageRef, err)
+	}
+	img, err := remote.Image(ref)
+	if err != nil {
+		return nil, fmt.Errorf("remote.Image() %q: %w", imageRef, err)
+	}
+	config, err := img.ConfigFile()
+	if err != nil {
+		return nil, fmt.Errorf("img.ConfigFile() %q: %w", imageRef, err)
+	}
+	return &config.Created.Time, nil
+}
